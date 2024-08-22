05ced3a94f4de1ce6b66f06d165d00c9fd4d8713
Alextheprogrammer0 <alejandrofloreselizarraras@gmail.com>
2024-08-22 08:33:18 -0600

    Add files via upload

---
+++ juego-de-preguntas-main/js/ver.js
@@ -0,0 +1,14 @@
+  /*  function ver() {
+        db.collection("usuarios").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                document.getElementById("print_name").innerHTML=`${doc.data().Nombre}`
+                document.getElementById("print_last").innerHTML=`${doc.data().Apellidos}`
+                document.getElementById("print_email").innerHTML=`${doc.data().Correo}`
+                document.getElementById("print_password").innerHTML=`${doc.data().Contraseña}`
+                document.getElementById("print_age").innerHTML=`${doc.data().Edad}`
+                
+            });
+        });
+    }
+    */
+    
